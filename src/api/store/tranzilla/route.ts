26ec50025112eab33abbe665e19b855cfd78fd18
Oryan Tayer <oryantayer@gmail.com>
2024-06-04 15:00:48 +0300

    add logs for tranzilla route

--- src/api/store/tranzilla/route.ts
+++ src/api/store/tranzilla/route.ts
@@ -1,17 +1,48 @@
 
 import { Request, Response } from 'express';
-import {headers} from "../../../admin/utils/tranzilla-headers"
+// import {headers} from "../../../admin/utils/tranzilla-headers"
 import axios from 'axios';
+
+
+function makeid(length) {
+    var result           = '';
+    var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    var charactersLength = characters.length;
+    for ( var i = 0; i < length; i++ ) {
+       result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
+ }
+
+
+ var time = Math.floor(Date.now() / 1000);
+ var nonce = makeid(80)
+ var key =
+   "XZyY6L8EwC4MiRZOAQdnEQ3Acq25ayXNOiITJzfHdPP1sRwKvjYp1VpeTWQwZheKIzTPLw9q0ln"
+ var privateKey = "tE07ITwV8o"
+ var hash = CryptoJS.HmacSHA256(key, privateKey + time + nonce).toString(
+   CryptoJS.enc.Hex
+ )
+
+
+export const headers = { 
+  'X-tranzila-api-app-key':key,
+  'X-tranzila-api-request-time':time,
+  'X-tranzila-api-nonce':nonce,
+  'X-tranzila-api-access-token':hash
+}
+
+
+
 
 export const POST = async (req: Request, res: Response) => {
     try {
         console.log("req.body", req.body);
-        
+        console.log("headers", headers);
         // Make the external API request
         const response = await axios.post('https://api.tranzila.com/v1/transaction/credit_card/create', req.body, {
             headers: headers
         });
-
         const data = response.data;
         // console.log(data);
 
